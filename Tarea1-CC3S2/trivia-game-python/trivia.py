313ce628135e1821993e7ebb4bd266e7787804c2
Alejandro <alejandrotaipe74@gmail.com>
2024-10-14 01:36:04 -0500

    quiz run quiz

--- Tarea1-CC3S2/trivia-game-python/trivia.py
+++ Tarea1-CC3S2/trivia-game-python/trivia.py
@@ -17,4 +17,21 @@ class Quiz:
             question = self.questions[self.current_question_index]
             self.current_question_index += 1
             return question
-        return None  
+        return None
+    def run_quiz():
+        quiz = Quiz()
+        while quiz.current_question_index < 10:
+            question = quiz.get_next_question()
+            if question:
+                print(question.description)
+                for idx, option in enumerate(question.options):
+                    print(f"{idx + 1}) {option}")
+                answer = input("Tu respuesta: ")
+                if quiz.answer_question(question, answer):
+                    print("¡Correcto!")
+                else:
+                    print("Incorrecto.")
+            else:
+                break
+        print(f"Juego terminado. Respuestas correctas: {quiz.correct_answers}, incorrectas:{quiz.incorrect_answers}")
+    
