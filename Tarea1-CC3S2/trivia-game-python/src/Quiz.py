faeef54ad2a60f384f6cf7ee314ab57e5d8765fc
Alejandro <alejandrotaipe74@gmail.com>
2024-10-24 00:49:04 -0500

    Testing class Question

---
+++ Tarea1-CC3S2/trivia-game-python/src/Quiz.py
@@ -0,0 +1,12 @@
+class Quiz:
+    def __init__(self):
+        self.questions = []
+        self.current_question_index = 0
+    def add_question(self, question):
+        self.questions.append(question)
+    def get_next_question(self):
+        if self.current_question_index < len(self.questions):
+            question = self.questions[self.current_question_index]
+            self.current_question_index += 1
+            return question
+        return None
