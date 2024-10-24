bb370f1016ff3b40888b6fb1219039027c2d2039
Alejandro <alejandrotaipe74@gmail.com>
2024-10-24 17:24:43 -0500

    app.py added

---
+++ Tarea1-CC3S2/trivia-game-python/src/questions_for_quiz.py
@@ -0,0 +1,52 @@
+questions_for_quiz = [
+    {
+        "description": "¿Cuánto es 5 + 7?",
+        "options": ["10", "11", "12", "13"],
+        "correct_answer": "12"
+    },
+    {
+        "description": "¿Cuál es el cuadrado de 3?",
+        "options": ["6", "8", "9", "12"],
+        "correct_answer": "9"
+    },
+    {
+        "description": "¿Cuánto es 100 ÷ 4?",
+        "options": ["25", "20", "30", "40"],
+        "correct_answer": "25"
+    },
+    {
+        "description": "¿Cuál es la raíz cuadrada de 81?",
+        "options": ["7", "8", "9", "10"],
+        "correct_answer": "9"
+    },
+    {
+        "description": "¿Cuánto es 15 x 2?",
+        "options": ["25", "30", "35", "40"],
+        "correct_answer": "30"
+    },
+    {
+        "description": "¿Cuál es el valor absoluto de -10?",
+        "options": ["10", "5", "15", "20"],
+        "correct_answer": "10"
+    },
+    {
+        "description": "¿Cuánto es 7 elevado a la potencia de 2?",
+        "options": ["49", "42", "50", "45"],
+        "correct_answer": "49"
+    },
+    {
+        "description": "¿Cuál es el resultado de 20 - 5?",
+        "options": ["10", "15", "25", "20"],
+        "correct_answer": "15"
+    },
+    {
+        "description": "¿Cuánto es 6 x 7?",
+        "options": ["36", "40", "42", "45"],
+        "correct_answer": "42"
+    },
+    {
+        "description": "¿Cuánto es 50 ÷ 5?",
+        "options": ["5", "10", "15", "20"],
+        "correct_answer": "10"
+    }
+]
