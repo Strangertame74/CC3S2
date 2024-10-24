d48cd7181fe2eba78e27cb3ee5d5824c3299da56
Alejandro <alejandrotaipe74@gmail.com>
2024-10-24 01:04:50 -0500

    Testing Quiz

--- Tarea1-CC3S2/trivia-game-python/src/Quiz.py
+++ Tarea1-CC3S2/trivia-game-python/src/Quiz.py
@@ -2,6 +2,8 @@ class Quiz:
     def __init__(self):
         self.questions = []
         self.current_question_index = 0
+        self.correct_answers = 0
+        self.incorrect_answers = 0
     def add_question(self, question):
         self.questions.append(question)
     def get_next_question(self):
@@ -10,3 +12,10 @@ class Quiz:
             self.current_question_index += 1
             return question
         return None
+    def answer_question(self, question, answer):
+        if question.is_correct(answer):
+            self.correct_answers += 1
+            return True
+        else:
+            self.incorrect_answers += 1
+            return False
